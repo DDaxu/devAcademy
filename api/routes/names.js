c7c7d123d3a43aef3d3180962c42547c611e09a5
DDaxu <akselikoskela93@gmail.com>
2021-01-23 21:50:46 +0200

    API update and prettier was added
    API update with better solutions and Prettier was runned for smoother looking code

--- api/routes/names.js
+++ api/routes/names.js
@@ -1,47 +1,43 @@
-const express = require('express');
-const router =  express.Router();
-const fs = require('fs');
-const nameList = fs.readFileSync('./names.json');
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const nameList = fs.readFileSync("./names.json");
 var list = JSON.parse(nameList);
 
 //Order by amount
-router.get('/byAmount', (req,res,next) =>{
-    var nameAmount = list.names.sort((a,b) => b.amount - a.amount);
-    res.status(200).json({
-         nameAmount
-    });
+router.get("/byAmount", (req, res) => {
+  var sortedByAmount = list.names.sort((a, b) => b.amount - a.amount);
+  res.status(200).json({
+    sortedByAmount
+  });
 });
 
 //Order by names
-router.get('/byNames', (req,res,next) =>{
-    var orderNames = list.names.sort((a, b) => a.name.localeCompare(b.name));
-    res.status(200).json({
-         orderNames
-    });
+router.get("/byNames", (req, res) => {
+  var sortedAlphabetically = list.names.sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+  res.status(200).json({
+    sortedAlphabetically
+  });
 });
 
 //Get total amount of names
-router.get('/totalAmount', (req,res,next) =>{
-    var sum = 0;
-    list.names.forEach(function(x){
-    
-       // console.log(x.amount);
-        sum = sum + x.amount;
-    });
-    res.status(200).json({
-        amount: sum
-   });
+router.get("/totalAmount", (req, res) => {
+  const sum = list.names.reduce(function (a, b) {
+    return a + b.amount;
+  }, 0);
+  res.status(200).json({
+    totalAmount: sum
+  });
 });
 //Get one amount of names
-router.get('/:personAmount', (req,res,next) =>{
-    var person = '';
-    list.names.forEach(function(x){
-        if(x.name === req.params.personAmount){
-             person = x.amount;
-        }
-     });
-     res.status(200).json({
-        person: person
-    });
+router.get("/:personAmount", (req, res) => {
+  const person = list.names.find(
+    ({ name }) => name === req.params.personAmount
+  );
+  res.status(200).json({
+    amount: person.amount
+  });
 });
 module.exports = router;
